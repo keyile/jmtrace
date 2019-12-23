e869fac4d90c69f7c4d86508c2983db38ec44a89
siyuan <xinfeiyang-2008@163.com>
2019-12-23 19:24:33 +0800

    merge

---
+++ src/main/java/mtrace/ClassInstrumenter.java
@@ -0,0 +1,45 @@
+package mtrace;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.lang.instrument.ClassFileTransformer;
+import java.security.ProtectionDomain;
+
+public class ClassInstrumenter implements ClassFileTransformer {
+    public static void main(final String[] args) throws Exception {
+        FileInputStream is = new FileInputStream("Test.class");
+
+        ClassReader cr = new ClassReader(is);
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        ClassAdapter ct = new ClassAdapter(cw);
+
+        cr.accept(ct, 0);
+
+        FileOutputStream fos = new FileOutputStream("Test.class");
+        fos.write(cw.toByteArray());
+        fos.close();
+    }
+
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
+                            ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+        if (!className.equalsIgnoreCase("Test")) {
+            return null;
+        }
+
+        System.out.println("transform Class     :" + className);
+
+        ClassReader cr = new ClassReader(classfileBuffer);
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        ClassAdapter ct = new ClassAdapter(cw);
+
+        cr.accept(ct, 0);
+
+        return cw.toByteArray();
+    }
+
+}
+
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,9 +1,12 @@
+
 public class Test{
 	public static void main(String[] args) {
 //		int[] a = new int [10];
 //		for (int i = 0; i < a.length; i++) {
 //			a[i] = 0;
 //		}
+		System.out.println("main");
+
 		A a = new A();
 		double zz = a.f;
 		char cc = A.c;
