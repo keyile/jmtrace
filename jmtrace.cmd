e62f60169833dca0d10c65e935a0c7519d9dc4c7
siyuan <xinfeiyang-2008@163.com>
2019-12-24 10:59:23 +0800

    jar

---
+++ jmtrace.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+java -javaagent:target/traceagent.jar %*
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,5 +1,5 @@
 
-public class Test{
+public class TestMain {
 	public static void main(String[] args) {
 //		int[] a = new int [10];
 //		for (int i = 0; i < a.length; i++) {
